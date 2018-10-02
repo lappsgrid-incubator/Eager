39e4de8887e80c6ff4a08fe0b54a2a49d1cd6838
Keith Suderman <suderman@cs.vassar.edu>
2018-10-01 23:34:07 -0400

    Added missing files

---
+++ retrieval/src/main/groovy/org/lappsgrid/eager/mining/retrieval/RedisLoader.groovy
@@ -0,0 +1,83 @@
+package org.lappsgrid.eager.mining.retrieval
+
+import org.lappsgrid.eager.core.Configuration
+import org.lappsgrid.eager.rabbitmq.Message
+import org.lappsgrid.eager.rabbitmq.topic.MailBox
+import org.lappsgrid.eager.rabbitmq.topic.PostOffice
+//import org.lappsgrid.serialization.Serializer
+
+import com.lambdaworks.redis.*;
+
+/**
+ *
+ */
+class RedisLoader {
+
+    /**
+     * Semaphore object used to block the main thread until a shutdown
+     * message has been received.
+     */
+    private Object semaphore
+
+    RedisClient redis
+
+    Configuration config
+    PostOffice office
+    MailBox box
+
+    RedisLoader() {
+        this(new Configuration())
+    }
+
+    RedisLoader(Configuration configuration) {
+        this.config = configuration
+        office = new PostOffice(config.POSTOFFICE)
+        RedisURI uri = RedisURI.create("redis://password@localhost:6379")
+        redis = new RedisClient(uri)
+    }
+
+    void start() {
+        box = new MailBox(config.properties, config.BOX_REDIS) {
+            @Override
+            void recv(String message) {
+                process(message)
+            }
+        }
+    }
+
+    void process(String json) {
+        Message message = Serializer.parse(json, Message)
+        if (message.command == 'shutdown') {
+            // Notify the main thread that they are done waiting.
+            synchronized (semaphore) {
+                semaphore.notifyAll()
+            }
+        }
+        else {
+            println json
+        }
+
+    }
+
+    void close() {
+        office.close()
+        box.close()
+    }
+
+    private void error(String message) {
+        office.send(config.BOX_ERROR, message)
+    }
+
+    public static void main(String[] args) {
+        RedisLoader fetch = new RedisLoader()
+        fetch.start()
+
+        // Wait forever for another thread to notify us that we should exit.
+        synchronized (fetch.semaphore) {
+            fetch.semaphore.wait()
+        }
+        fetch.close()
+
+
+    }
+}
